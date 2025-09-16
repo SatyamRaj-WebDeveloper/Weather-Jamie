64961ec3f5621e92f83925cc55006dd8cb6e3cbb
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2025-09-16 11:37:40 +0530

    Deployment error fixed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import SearchBar from "../components/SearchBar";
 import WeatherCard from "../components/WeatherCard";
 import ErrorMessage from "../components/ErrorMessage";
 import Loader from "../components/Loader";
-import { useFetchWeather } from "../hooks/useFetchWeather";
+import { useFetchWeather } from "../hooks/UseFetchWeather.js";
 
 export default function Home() {
   const { weather, loading, error, fetchWeather } = useFetchWeather();
